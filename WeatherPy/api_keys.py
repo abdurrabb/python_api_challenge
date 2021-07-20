079eb488bc2f16595183ed1296bffbb5e7244256
abdurrabb <mr.ab.watkins@gmail.com>
2021-07-20 09:34:22 -0400

    update gitignore

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,2 +1,3 @@
-g_key = AIzaSyCqh-nFV__XYqJoxH8bp-wVuVEMCRv0LtY
-weather_api_key = Fyckud-gizwag-2tisde
+g_key = "AIzaSyCqh-nFV__XYqJoxH8bp-wVuVEMCRv0LtY"
+
+weather_key = "53f6b7060897a60d43048c605687480e"
